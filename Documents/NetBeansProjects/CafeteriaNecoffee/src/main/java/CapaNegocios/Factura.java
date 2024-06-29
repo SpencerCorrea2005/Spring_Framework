6a8e8e2cc463c9e08a3ea8d04acb00e84aaadc26
Spencer Correa <spenceruni2005@gmail.com>
2024-06-29 00:05:05 -0500

    "Se crea la clase Factura. Se trabajó hasta el 29/06/2024."

---
+++ Documents/NetBeansProjects/CafeteriaNecoffee/src/main/java/CapaNegocios/Factura.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CapaNegocios;
+
+/**
+ *
+ * @author USUARIO PC
+ */
+public class Factura {
+    private int totalProductos;
+    private int cantidadPagada;
+    private int devuelta;
+    
+}
