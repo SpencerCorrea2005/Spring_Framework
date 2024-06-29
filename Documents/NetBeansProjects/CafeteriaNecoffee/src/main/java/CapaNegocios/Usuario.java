71945a81bb0a3cf914bf214fb7f7c299b8a27e87
Spencer Correa <spenceruni2005@gmail.com>
2024-06-28 22:54:51 -0500

    "Creación de la clase Usuario, junto con sus atributos y métodos"

---
+++ Documents/NetBeansProjects/CafeteriaNecoffee/src/main/java/CapaNegocios/Usuario.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/*
+* Nombre del paquete (Capa de Negocios del sistema)
+*/
+package CapaNegocios;
+
+/**
+ *
+ * @author USUARIO PC
+ */
+public class Usuario {
+    
+    //Atributos
+    protected int numDocIdentidad;
+    protected String nombreUsuario;
+    protected int telefono;
+    protected String direccionDomicilio;
+    
+    //Métodos
+    
+    //Método constructor
+    
+    public Usuario(int numDocIdentidad, String nombreUsuario, int telefono, String direccionDomicilio) {
+        this.numDocIdentidad = numDocIdentidad;
+        this.nombreUsuario = nombreUsuario;
+        this.telefono = telefono;
+        this.direccionDomicilio = direccionDomicilio;
+    }
+    
+    //Métodos Setters y Getters
+    
+    public void setNumDocIdentidad(int numDocIdentidad) {
+        this.numDocIdentidad = numDocIdentidad;
+    }
+
+    public int getNumDocIdentidad() {
+        return numDocIdentidad;
+    }
+    
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+    
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+    
+    public void setDireccionDomicilio(String direccionDomicilio) {
+        this.direccionDomicilio = direccionDomicilio;
+    }
+
+    public String getDireccionDomicilio() {
+        return direccionDomicilio;
+    }
+    
+    //Otros métodos
+    
+    public void ingresarPagina() {
+        System.out.println("Bienvenido a la página" + getNombreUsuario());
+    }
+    
+    public void registrarse() {
+        System.out.println("A continuación, te vas a registrar en la página web de la cafetería.");
+    }
+    
+}
