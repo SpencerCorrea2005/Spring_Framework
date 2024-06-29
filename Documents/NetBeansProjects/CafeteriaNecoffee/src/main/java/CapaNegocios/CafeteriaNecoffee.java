69de564d906eea18074d3ac9cf2312e06e1071c3
Spencer Correa <spenceruni2005@gmail.com>
2024-06-28 22:57:36 -0500

    "Creación de la clase con el método main"

---
+++ Documents/NetBeansProjects/CafeteriaNecoffee/src/main/java/CapaNegocios/CafeteriaNecoffee.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package CapaNegocios;
+
+/**
+ *
+ * @author USUARIO PC
+ */
+public class CafeteriaNecoffee {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
