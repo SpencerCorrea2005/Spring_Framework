3a8bb443ee6d52699fc8378e77473fb200b2c4b4
Spencer Correa <spenceruni2005@gmail.com>
2024-06-29 22:26:22 -0500

    "Se agregan sus atributos con sus respectivos métodos."

--- Documents/NetBeansProjects/CafeteriaNecoffee/src/main/java/CapaNegocios/Factura.java
+++ Documents/NetBeansProjects/CafeteriaNecoffee/src/main/java/CapaNegocios/Factura.java
@@ -9,8 +9,85 @@ package CapaNegocios;
  * @author USUARIO PC
  */
 public class Factura {
+    private int ID_Factura;
     private int totalProductos;
     private int cantidadPagada;
     private int devuelta;
+    private String domicilioEmpresa;
     
+    //Método constructor
+
+    public Factura(int ID_Factura, int totalProductos, int cantidadPagada, int devuelta, String domicilioEmpresa) {
+        this.ID_Factura = ID_Factura;
+        this.totalProductos = totalProductos;
+        this.cantidadPagada = cantidadPagada;
+        this.devuelta = devuelta;
+        this.domicilioEmpresa = domicilioEmpresa;
+    }
+    
+    //Métodos set y get
+
+    public int getID_Factura() {
+        return ID_Factura;
+    }
+
+    public void setID_Factura(int ID_Factura) {
+        this.ID_Factura = ID_Factura;
+    }
+
+    public int getTotalProductos() {
+        return totalProductos;
+    }
+
+    public void setTotalProductos(int totalProductos) {
+        this.totalProductos = totalProductos;
+    }
+
+    public int getCantidadPagada() {
+        return cantidadPagada;
+    }
+
+    public void setCantidadPagada(int cantidadPagada) {
+        this.cantidadPagada = cantidadPagada;
+    }
+
+    public int getDevuelta() {
+        return devuelta;
+    }
+
+    public void setDevuelta(int devuelta) {
+        this.devuelta = devuelta;
+    }
+
+    public String getDomicilioEmpresa() {
+        return domicilioEmpresa;
+    }
+
+    public void setDomicilioEmpresa(String domicilioEmpresa) {
+        this.domicilioEmpresa = domicilioEmpresa;
+    }
+    
+    //Métodos adicionales
+    
+    public void emitirFactura() {
+        System.out.println("""
+                           Estimado usuario. Los datos de su factura son: 
+                           ID_Factura: """ + ID_Factura + 
+                            ".\nValor total de la compra realizada" + totalProductos +
+                            "\nLa cantidad pagada es: " + cantidadPagada +
+                            "\nSu devuelta es: " + devuelta +
+                            "\nEl domicilio de la empresa es: " + domicilioEmpresa);
+    }
+    
+    public void guardarFactura() {
+        System.out.println("Factura guardada con éxito");
+    }
+    
+    public void registrarPedido() {
+        System.out.println("Pedido registrado");
+    }
+    
+    public void registropagoRealizado() {
+        System.out.println("Se ha registrado el pago realizado con éxito dentro del sistema");
+    }
 }
