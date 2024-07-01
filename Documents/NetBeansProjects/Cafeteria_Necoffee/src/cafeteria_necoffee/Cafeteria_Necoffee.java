95962aa22256d2bb7a80da92dddf13360d5ab15e
Spencer Correa <spenceruni2005@gmail.com>
2024-06-30 21:29:04 -0500

    "Se agregaron los datos de la base de datos correctamente"

--- Documents/NetBeansProjects/Cafeteria_Necoffee/src/cafeteria_necoffee/Cafeteria_Necoffee.java
+++ Documents/NetBeansProjects/Cafeteria_Necoffee/src/cafeteria_necoffee/Cafeteria_Necoffee.java
@@ -36,15 +36,16 @@ public class Cafeteria_Necoffee {
             statement = conexion.createStatement();
             
             //statement.executeUpdate("INSERT INTO Usuario(NumDocumentoID, NombreUsuario, ApellidosUsuario, TipoDocumentoID, Telefono) VALUES ('1578963024', 'Andres Felipe', 'Osorio Barbosa', 'Cédula de Ciudadanía', '3204893510')");
-            
+            //statement.executeUpdate("UPDATE Usuario SET TipoDocumentoID = 'Cedula de Ciudadania'");
+            //statement.executeUpdate("DELETE FROM Usuario WHERE NombreUsuario='Samuel'");
+
             rs = statement.executeQuery("SELECT * FROM usuario");
             rs.next();
             
+            
             System.out.println("NumDocumentoID" + " | " + "NombreUsuario" + " | " + "ApellidosUsuario" + " | " + "TipoDocumentoID" + " | " + "Telefono");
             do {
-                System.out.println(rs.getInt("NumDocumentoID") + " | " + rs.getString("NombreUsuario")); 
-                
-                //+ " | " + rs.getString("ApellidosUsuario") + " | " + rs.getString("TipoDocumentoID") + rs.getInt("Telefono"));
+                System.out.println(rs.getInt("NumDocumentoID") + " | " + rs.getString("NombreUsuario") + " | " + rs.getString("ApellidosUsuario") + " | " + rs.getString("TipoDocumentoID") + " | " + rs.getString("Telefono"));
             } while(rs.next());
             
         } catch (SQLException ex) {
