bc2879c2c97ff98a0bb80c58cf68c123e06e1dc9
Spencer Correa <spenceruni2005@gmail.com>
2024-07-10 14:50:58 -0500

    "Se booró la respuesta en código HTML que tenía el servlet. Se guardó como un comentario al final por si se necesitara ese fragmento de código"

---
+++ Documents/NetBeansProjects/FormServlet/src/java/Controladores/RecibeDatos.java
@@ -0,0 +1,89 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package Controladores;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author USUARIO PC
+ */
+public class RecibeDatos extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response, String msg)
+            throws ServletException, IOException {
+        
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        String user = request.getParameter("Usuario");
+        String password = request.getParameter("Clave");
+        
+            if("admin".equals(user) && "1234".equals(password)) {
+                
+                processRequest(request, response, "Registro exitoso");
+            } else {
+                
+                processRequest(request, response, "Registro invalido");
+            }
+
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
+
+/*response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            /*out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet RecibeDatos</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet RecibeDatos at " + msg + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }*/
